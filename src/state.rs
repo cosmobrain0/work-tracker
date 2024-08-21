073ca49dfa69e79b09d07925f1d6edb9b89d26cc
cosmobrain0 <cosmobrain@outlook.com>
2024-08-21 14:49:13 +0100

    added comments to state.rs

--- src/state.rs
+++ src/state.rs
@@ -11,6 +11,7 @@ pub use payment::*;
 pub use project::*;
 pub use work_slice::*;
 
+/// Used to create, modify and delete projects.
 #[derive(Debug)]
 pub struct State {
     previous_project_id: u64,
@@ -18,6 +19,7 @@ pub struct State {
     projects: Vec<Project>,
 }
 impl State {
+    /// Returns an empty State, which has no projects.
     pub fn new() -> Self {
         Self {
             previous_project_id: 0,
@@ -26,6 +28,7 @@ impl State {
         }
     }
 
+    /// Creates a new project, and returns its ID.
     pub fn new_project(&mut self, name: String, description: String) -> ProjectId {
         let id = ProjectId::new(self.previous_project_id + 1);
 
@@ -46,10 +49,13 @@ impl State {
     }
 }
 impl State {
+    /// Returns the IDs of all projects.
     pub fn all_project_ids(&self) -> impl Iterator<Item = ProjectId> + '_ {
         self.projects.iter().map(|x| x.id())
     }
 
+    /// Tries to find a project based on its ID,
+    /// but fails if the project has been deleted.
     pub fn project_from_id(&self, id: ProjectId) -> Option<&Project> {
         self.projects.iter().find(|x| x.id() == id)
     }
@@ -58,6 +64,7 @@ impl State {
         self.projects.iter_mut().find(|x| x.id() == id)
     }
 
+    /// Returns all projects in its ID.
     pub fn all_projects(&self) -> impl Iterator<Item = &Project> + '_ {
         self.projects.iter()
     }
@@ -66,10 +73,14 @@ impl State {
         self.projects.iter_mut()
     }
 
+    /// Returns true if a project with the given ID exists,
+    /// and returns false if the project has been deleted.
     pub fn project_exists(&self, id: ProjectId) -> bool {
         self.all_project_ids().any(|x| x == id)
     }
 
+    /// Returns the project which the work slice specified by the given ID,
+    /// but fails if the work slice has been deleted, or if the project has been deleted.
     pub fn project_id_from_work_slice(&self, work_slice_id: WorkSliceId) -> Option<ProjectId> {
         self.all_projects()
             .map(|x| {
@@ -85,6 +96,8 @@ impl State {
     }
 }
 impl State {
+    /// Tries to start a new incomplete work slice for a project,
+    /// but can fail. See `WorkStartError` for information on how.
     pub fn start_work(
         &mut self,
         id: ProjectId,
@@ -102,6 +115,8 @@ impl State {
         }
     }
 
+    /// Tries to end the current incomplete work slice for a project,
+    // but can fail. See `WorkEndError` for more information on how.
     pub fn end_work(&mut self, id: ProjectId, time: DateTime<Utc>) -> Result<(), WorkEndError> {
         match self.project_from_id_mut(id) {
             Some(project) => project.complete_work(time).map_err(Into::into),
@@ -109,6 +124,8 @@ impl State {
         }
     }
 
+    /// Tries to delete a project, but can fail if the project has already been deleted.
+    /// Returns true if the project deletion is successful, or false otherwise.
     pub fn delete_project(&mut self, id: ProjectId) -> bool {
         let index = self
             .all_project_ids()
@@ -124,6 +141,8 @@ impl State {
         }
     }
 
+    /// Tries to delete a work slice from its project, but fails if the work slice has been deleted,
+    /// or if its project has been deleted. Returns true if it succeeds.
     pub fn delete_work_slice(&mut self, id: WorkSliceId) -> bool {
         match self.project_id_from_work_slice(id) {
             Some(project_id) => self.delete_work_slice_from_project(project_id, id),
@@ -131,6 +150,8 @@ impl State {
         }
     }
 
+    /// Tries to delete a work slice from the specified project, but fails if the work slice has been deleted,
+    /// or if it is not part of the project, or if the project has already been deleted.
     pub fn delete_work_slice_from_project(
         &mut self,
         project_id: ProjectId,
@@ -142,6 +163,8 @@ impl State {
     }
 }
 impl State {
+    /// tries to return the work slice from its ID (complete or incomplete)
+    /// but fails if it has been deleted or if its project has been deleted.
     pub fn work_slice_from_id(&self, id: WorkSliceId) -> Option<WorkSlice<'_>> {
         self.projects.iter().find_map(|x| x.work_slice_from_id(id))
     }
