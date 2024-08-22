a4393cdbcc97efbab965df9ea133934498241873
cosmobrain0 <cosmobrain@outlook.com>
2024-08-22 18:33:56 +0100

    completed the view command (view all)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod state;
 
-use std::error::Error;
 use std::io::ErrorKind;
+use std::{error::Error, str::FromStr};
 
+use chrono::{DateTime, Duration, TimeDelta};
 use clap::{Parser, Subcommand};
 use state::{
-    Change, CompleteWorkSliceData, IncompleteWorkSliceData, Project, ProjectData, ProjectId, State,
-    WorkSlice, WorkSliceId,
+    Change, CompleteWorkSlice, CompleteWorkSliceData, IncompleteWorkSlice, IncompleteWorkSliceData,
+    Money, Project, ProjectData, ProjectId, State, WorkSlice, WorkSliceId,
 };
 
-use crate::state::Payment;
+use crate::state::{MoneyExact, Payment};
 
 #[derive(Parser)]
 struct Cli {
@@ -50,7 +51,10 @@ enum DeleteCommand {
 
 #[derive(Subcommand)]
 enum ViewCommand {
-    All,
+    All {
+        #[arg(short, long)]
+        verbose: bool,
+    },
     Project {
         project_id: u64,
         #[arg(short, long)]
@@ -62,11 +66,11 @@ enum ViewCommand {
 }
 
 fn main() -> Result<(), ()> {
-    let cli = Cli::parse();
-
     dotenvy::dotenv().expect("Couldn't load .env");
     let save_file_name = std::env::var("SAVE_FILE").expect("Couldn't load the SAVE_FILE variable");
     let initial_data = load_data(&save_file_name).expect("Failed to load data");
+
+    let cli = Cli::parse();
 
     let mut state = State::new(initial_data.clone(), move |changes, final_data| {
         save_data(&save_file_name, changes, final_data);
@@ -107,11 +111,43 @@ fn main() -> Result<(), ()> {
             },
         },
         Command::View { command } => match command {
-            ViewCommand::All => todo!(),
+            ViewCommand::All { verbose } => {
+                if verbose {
+                    println!(
+                        "{}",
+                        state
+                            .all_projects()
+                            .map(format_project_verbose)
+                            .reduce(|acc, e| format!("{acc}\n\n{e}"))
+                            .unwrap_or_else(|| "No current projects.".to_string())
+                    );
+                } else {
+                    println!(
+                        "{}",
+                        state
+                            .all_projects()
+                            .map(format_project_not_verbose)
+                            .reduce(|acc, e| format!("{acc}\n\n{e}"))
+                            .unwrap_or_else(|| "No current projects.".to_string())
+                    );
+                }
+            }
             ViewCommand::Project {
                 project_id,
                 verbose,
-            } => todo!(),
+            } => match state.project_from_id(unsafe { ProjectId::new(project_id) }) {
+                Some(project) => {
+                    println!(
+                        "{}",
+                        if verbose {
+                            format_project_verbose(project)
+                        } else {
+                            format_project_not_verbose(project)
+                        }
+                    );
+                }
+                None => eprintln!("That project id ({project_id}) is invalid!"),
+            },
             ViewCommand::Work { work_slice_id } => {
                 match state.work_slice_from_id(unsafe { WorkSliceId::new(work_slice_id) }) {
                     Some(WorkSlice::Complete(complete)) => {
@@ -123,7 +159,7 @@ fn main() -> Result<(), ()> {
                             Payment::Fixed(payment) => format!("fixed at {payment}"),
                         };
                         let start = complete.start().to_rfc2822();
-                        let duration = complete.duration().to_string();
+                        let duration = format_duration(complete.duration());
                         let total_payment = complete.calculate_payment().as_pence();
                         let total_payment_pounds = (total_payment / 100.0).floor().to_string();
                         let total_payment_pence = (total_payment % 100.0).floor().to_string();
@@ -138,19 +174,13 @@ fn main() -> Result<(), ()> {
                         let project_id = state
                             .project_id_from_work_slice(unsafe { WorkSliceId::new(work_slice_id) })
                             .unwrap();
-                        let payment = match incomplete.payment() {
-                            Payment::Hourly(rate) => format!("{rate} / hour"),
-                            Payment::Fixed(payment) => format!("fixed at {payment}"),
-                        };
+                        let payment = incomplete.payment();
                         let start = incomplete.start().to_rfc2822();
-                        let duration = incomplete.duration().to_string();
-                        let total_payment = incomplete.calculate_payment_so_far().as_pence();
-                        let total_payment_pounds = (total_payment / 100.0).floor().to_string();
-                        let total_payment_pence = (total_payment % 100.0).floor().to_string();
+                        let duration = format_duration(incomplete.duration()).to_string();
+                        let total_payment = incomplete.calculate_payment_so_far();
                         println!(
                             "Current work slice {work_slice_id} for project {project_id}: Payment is {payment} - started at {start}, lasting {duration} and earning {total_payment}",
                             project_id = unsafe { project_id.inner() },
-                            total_payment = format!("£{total_payment_pounds} and {total_payment_pence} pence"),
                         );
                     }
                     None => eprintln!("That work slice id ({work_slice_id}) is invalid!"),
@@ -208,4 +238,163 @@ fn save_data(file_name: &str, _changes: Vec<Change>, final_data: Vec<&Project>) 
         .expect("Failed to serialize data"),
     )
     .expect("Failed to save data");
+}
+
+fn format_complete_work_slice(work_slice: &CompleteWorkSlice) -> String {
+    format!(
+        "{id} - {start} - {end}, {payment}, {total_payment}",
+        id = unsafe { work_slice.id().inner() },
+        start = work_slice.start().to_rfc2822(),
+        end = work_slice.completion().to_rfc2822(),
+        payment = work_slice.payment(),
+        total_payment = work_slice.calculate_payment(),
+    )
+}
+
+fn format_incomplete_work_slice_verbose(work_slice: &IncompleteWorkSlice) -> String {
+    format!(
+        "{id} - started at {start}, {duration} ago, {payment} - {total_payment}",
+        id = unsafe { work_slice.id().inner() },
+        start = work_slice.start().to_rfc2822(),
+        duration = format_duration(work_slice.duration()),
+        payment = work_slice.payment(),
+        total_payment = work_slice.calculate_payment_so_far(),
+    )
+}
+
+fn format_incomplete_work_slice(work_slice: &IncompleteWorkSlice) -> String {
+    format!(
+        "{id} - started {duration} ago",
+        id = unsafe { work_slice.id().inner() },
+        duration = format_duration(work_slice.duration())
+    )
+}
+
+fn format_project_not_verbose(project: &Project) -> String {
+    let completed_work: Vec<_> = project.complete_work_slices().collect();
+    let incomplete_work: Option<&IncompleteWorkSlice> = project.current_work_slice();
+
+    let completed_work_string = {
+        let number = completed_work.len();
+        let list = completed_work
+            .iter()
+            .map(|x| x.id())
+            .map(|x| unsafe { x.inner().to_string() })
+            .reduce(|acc, e| format!("{acc}, {e}"))
+            .unwrap_or_default();
+        format!("- completed work: {number} [{list}]")
+    };
+    let incomplete_work_string = match &incomplete_work {
+        Some(work) => format!(
+            "{id} - started {duration} ago",
+            id = unsafe { work.id().inner() },
+            duration = format_duration(work.duration())
+        ),
+        None => "not working".to_string(),
+    };
+
+    let total_duration = format_duration(
+        completed_work
+            .iter()
+            .map(|x| x.duration())
+            .sum::<TimeDelta>()
+            + incomplete_work
+                .map(|x| x.duration())
+                .unwrap_or(TimeDelta::zero()),
+    );
+    let total_payment = completed_work
+        .iter()
+        .map(|x| x.calculate_payment())
+        .sum::<MoneyExact>()
+        + incomplete_work
+            .map(|x| x.calculate_payment_so_far())
+            .unwrap_or(MoneyExact::new(0.0).unwrap());
+    let top_line = format!(
+        "Project {id}: {name} ({total_duration}, {total_payment})",
+        id = unsafe { project.id().inner() },
+        name = project.name(),
+    );
+    format!("{top_line}\n{completed_work_string}\n- current work: {incomplete_work_string}")
+}
+
+fn format_project_verbose(project: &Project) -> String {
+    let completed_work: Vec<_> = project.complete_work_slices().collect();
+    let incomplete_work: Option<&IncompleteWorkSlice> = project.current_work_slice();
+
+    let completed_work_string = {
+        let data = completed_work
+            .iter()
+            .map(|x| format!("  {}", format_complete_work_slice(x)))
+            .reduce(|acc, e| format!("{acc}\n{e}"));
+        match data {
+            Some(x) => format!("- completed work: {len}\n{x}", len = completed_work.len()),
+            None => "- no completed work".to_string(),
+        }
+    };
+    let incomplete_work_string = match &incomplete_work {
+        Some(work) => format!(
+            "{id} - started at {start}, {duration} ago, {payment} - {total_payment}",
+            id = unsafe { work.id().inner() },
+            start = work.start().to_rfc2822(),
+            duration = format_duration(work.duration()),
+            payment = work.payment(),
+            total_payment = work.calculate_payment_so_far()
+        ),
+        None => "not owrking".to_string(),
+    };
+
+    let total_duration = format_duration(
+        completed_work
+            .iter()
+            .map(|x| x.duration())
+            .sum::<TimeDelta>()
+            + incomplete_work
+                .map(|x| x.duration())
+                .unwrap_or(TimeDelta::zero()),
+    );
+    let total_payment = completed_work
+        .iter()
+        .map(|x| x.calculate_payment())
+        .sum::<MoneyExact>()
+        + incomplete_work
+            .map(|x| x.calculate_payment_so_far())
+            .unwrap_or(MoneyExact::new(0.0).unwrap());
+    let top_line = format!(
+        "Project {id}: {name} ({total_duration}, {total_payment})",
+        id = unsafe { project.id().inner() },
+        name = project.name(),
+    );
+    let description = project.description();
+    format!("{top_line}\n{description}\n{completed_work_string}\n- current work: {incomplete_work_string}")
+}
+
+fn format_duration(duration: Duration) -> String {
+    let hours = duration.num_seconds() / (60 * 60);
+    let minutes = (duration.num_seconds() / 60) % 60;
+    let seconds = duration.num_seconds() % 60;
+    let hours = if hours == 0 {
+        None
+    } else {
+        Some(format!("{hours} hours"))
+    };
+    let minutes = if minutes == 0 {
+        None
+    } else {
+        Some(format!("{minutes} minutes"))
+    };
+    let seconds = if seconds == 0 {
+        None
+    } else {
+        Some(format!("{seconds} seconds"))
+    };
+    match (hours, minutes, seconds) {
+        (None, None, None) => "0 seconds".to_string(),
+        (None, None, Some(s)) => s,
+        (None, Some(m), None) => m,
+        (None, Some(m), Some(s)) => format!("{m} and {s}"),
+        (Some(h), None, None) => h,
+        (Some(h), None, Some(s)) => format!("{h} and {s}"),
+        (Some(h), Some(m), None) => format!("{h} and {m}"),
+        (Some(h), Some(m), Some(s)) => format!("{h}, {m} and {s}"),
+    }
 }
